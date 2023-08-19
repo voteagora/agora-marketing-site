4ca2c7d9bb60f4499eeac25b4e4605b405ad405d
Yitong <zha.yitong@gmail.com>
2023-08-19 16:06:59 -0700

    quick check in

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,182 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className="bg-gray-50">
+      <header></header>
+      <main className="container mx-auto max-w-screen-lg bg-white px-8 pt-12">
+        <section>
+          <div className="flex flex-col gap-3 max-w-xl">
+            <h4>Governance infra for onchain communities</h4>
+            <h1>
+              Deploy a production-ready governance stack – from no governance to
+              thriving ecosystem.
+            </h1>
+            <p>
+              We provide onchain organizations with the tools they need to make
+              quick, effective, and permissionless decisions that help their
+              communities grow. Agora handles the infra so you can focus on
+              building your protocol.
+            </p>
+            
+          </div>
+          <button className="py-2 px-4 mt-6 bg-neutral-950 text-white rounded">
+            Get in touch
+          </button>
+          <Image
+            src="/op-agora.png"
+            width={1000}
+            height={1000}
+            alt="Picture of the Optimism Agora"
+            className="w-full border rounded-lg my-6"
+          />
+        </section>
+        <section className="mb-12">
+        <h4>Trusted by</h4>
+          <div className="flex justify-between py-6 px-12 rounded border">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/logo-optimism.png"
+              width={120}
+              height={90}
+              alt="ENS logo"
+              className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 transition"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            <Image
+              src="/logo-uniswap.png"
+              width={120}
+              height={90}
+              alt="ENS logo"
+              className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 transition"
+            />
+            <Image
+              src="/logo-ens.png"
+              width={120}
+              height={90}
+              alt="ENS logo"
+              className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 transition"
+            />
+            <Image
+              src="/logo-nouns.png"
+              width={120}
+              height={90}
+              alt="ENS logo"
+              className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 transition"
+            />
+            <Image
+              src="/logo-lyra.png"
+              width={120}
+              height={90}
+              alt="ENS logo"
+              className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 transition"
+            />
+          </div>
+        </section>
+        <section>
+          <h1>What we offer: Solid foundation to enable ecosystem growth.</h1>
+          <p>
+            We provide a strong governance foundation that enables your team and
+            community to focus on growing your protocol. Spend your time growing
+            your protocol and community, not chasing after voters.
           </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <h2>Today</h2>
+          <ul>
+            <li>Hard to attract new delegates, contributors</li>
+            <li>Operational nightmare shepherding proposals through</li>
+            <li>
+              Hard to maintain credible decentralization: correct votable
+              supply, good token distributiom
+            </li>
+          </ul>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <h2>With Agora</h2>
+          <ul>
+            <li>
+              Our network brings in contributors, delegates and fresh talent to
+              your ecosystem.{" "}
+            </li>
+            <li>
+              We handle the infra while you focus on tuning the incentives and
+              economics to growing an ecosystem your community loves
+            </li>
+            <li>
+              Clear tracking of decentralization metrics and community KPIs
+            </li>
+          </ul>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <section>
+          <p>
+            Running our first delegate race with Agora, we were able to offload
+            so much of the operational headaches to them. We attracted 3x more
+            active delegates in 1 month than we had in the last 3 years. On top
+            of this, we were able to increase votable supply by 25% in the
+            ecosystem, this is a crucial step in our path towards decentralizing
+            Uniswap.
           </p>
-        </a>
-      </div>
-    </main>
-  )
+          <p>Devin Walsh, Uniswap Foundation</p>
+        </section>
+
+        <section>
+          <h1>Ecosystem Growth Package</h1>
+          <div>Already have a decentralized community?</div>
+          <div>
+            Agora assists you in decentralization from end-to-end. All-in-one
+            operating system to source, attract, and collaborate with your
+            delegates and ecosystem contributors.
+          </div>
+
+          <div>
+            <h2>Governance application stack</h2>
+            <div>
+              <h3>Governor upgrades</h3>
+              <p>
+                Deploy Agora governor in one-click. Flexible and customizable
+                parameters and modules available.
+              </p>
+              <img src="https://placehold.co/600x400" alt="" />
+              <ul>
+                <li>
+                  Partial delegation. No more splitting up multi-sigs to run
+                  programs.
+                </li>
+                <li>
+                  Liquid delegation. Enable time-bound, split rights and
+                  re-delegation capabilities.
+                </li>
+                <li>
+                  Quorum management. Dynamic quorums, flexible quorums, based on
+                  what you need.
+                </li>
+                <li>
+                  Multiple voting systems. Standard voting (Yes/No), Approval
+                  Voting, Quadratic Voting, customized to your governance needs.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Agora Governor Client</h3>
+              <p>
+                Beautiful delegation and voting interface. Easy to create
+                proposal creation.
+              </p>
+              <img src="https://placehold.co/600x400" alt="" />
+              <ul>
+                <li>
+                  White label solutions. We take care of the infrastructure and
+                  hosting, tailored to your design system on your domains.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <h2>Ecosystem growth tooling</h2>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
