8d29090dfc529ed93d23e3f66c626f512972bb32
Yitong <zha.yitong@gmail.com>
2023-11-20 16:40:58 -0800

    fixed favicon

--- components/jobs.tsx
+++ components/jobs.tsx
@@ -31,44 +31,60 @@ const Jobs = ({ posts }: Props) => {
       </div>
       <div className="mt-8 flex flex-row justify-between">
         <div className="w-1/6 p-1 -rotate-8 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
-          <img
-            className="rounded-sm"
-            src={
-              "https://media.licdn.com/dms/image/C5603AQFcnn7Ff-baQw/profile-displayphoto-shrink_400_400/0/1549004768214?e=1706140800&v=beta&t=lTrDYh8x9WXnPK8oS5qxWBPGHFYGppQFgSmpsC5Om64"
-            }
-            alt={"yitong"}
-          ></img>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">Yitong</div>
+          <a target="_blank" href="https://twitter.com/zhayitong">
+            <img
+              className="rounded-sm"
+              src={
+                "https://media.licdn.com/dms/image/C5603AQFcnn7Ff-baQw/profile-displayphoto-shrink_400_400/0/1549004768214?e=1706140800&v=beta&t=lTrDYh8x9WXnPK8oS5qxWBPGHFYGppQFgSmpsC5Om64"
+              }
+              alt={"yitong"}
+            ></img>
+            <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+              Yitong
+            </div>
+          </a>
         </div>
         <div className="w-1/6 p-1 rotate-12 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
-          <img
-            className="rounded-sm"
-            src={
-              "https://media.licdn.com/dms/image/D5603AQFmfX8p31p7UA/profile-displayphoto-shrink_400_400/0/1700497855391?e=1706140800&v=beta&t=IpHZbqVJno0dwhJMVCET_CUjNEzLqaUhmEQbc_lNvwc"
-            }
-            alt={"kent"}
-          ></img>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">Kent</div>
+          <a target="_blank" href="https://www.linkedin.com/in/kentfenwick/">
+            <img
+              className="rounded-sm"
+              src={
+                "https://media.licdn.com/dms/image/D5603AQFmfX8p31p7UA/profile-displayphoto-shrink_400_400/0/1700497855391?e=1706140800&v=beta&t=IpHZbqVJno0dwhJMVCET_CUjNEzLqaUhmEQbc_lNvwc"
+              }
+              alt={"kent"}
+            ></img>
+            <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+              Kent
+            </div>
+          </a>
         </div>
         <div className="w-1/6 p-1 -rotate-6 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
-          <img
-            className="rounded-sm"
-            src={
-              "https://media.licdn.com/dms/image/C5603AQGODXf8t9gi7A/profile-displayphoto-shrink_400_400/0/1651604241523?e=1706140800&v=beta&t=M9fX95V-HAErmU0AScztWv398H7piXngxHuA1n35ehw"
-            }
-            alt={"charlie"}
-          ></img>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">Charlie</div>
+          <a target="_blank" href="https://www.linkedin.com/in/charliecf/">
+            <img
+              className="rounded-sm"
+              src={
+                "https://media.licdn.com/dms/image/C5603AQGODXf8t9gi7A/profile-displayphoto-shrink_400_400/0/1651604241523?e=1706140800&v=beta&t=M9fX95V-HAErmU0AScztWv398H7piXngxHuA1n35ehw"
+              }
+              alt={"charlie"}
+            ></img>
+            <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+              Charlie
+            </div>
+          </a>
         </div>
         <div className="w-1/6 p-1 rotate-8 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
-          <img
-            className="rounded-sm"
-            src={
-              "https://media.licdn.com/dms/image/C4D03AQEOjFe_Ox8BeQ/profile-displayphoto-shrink_400_400/0/1524421548782?e=1706140800&v=beta&t=pRrhm-rvB34gtF7c2g1mGZfK0_v3FtXW1qDl8a1kvWM"
-            }
-            alt={"stepan"}
-          ></img>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">Stepan</div>
+          <a target="_blank" href="https://www.linkedin.com/in/sarsentjev/">
+            <img
+              className="rounded-sm"
+              src={
+                "https://media.licdn.com/dms/image/C4D03AQEOjFe_Ox8BeQ/profile-displayphoto-shrink_400_400/0/1524421548782?e=1706140800&v=beta&t=pRrhm-rvB34gtF7c2g1mGZfK0_v3FtXW1qDl8a1kvWM"
+              }
+              alt={"stepan"}
+            ></img>
+            <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+              Stepan
+            </div>
+          </a>
         </div>
         <div className="w-1/6 p-1 -rotate-6 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
           <img
@@ -76,13 +92,17 @@ const Jobs = ({ posts }: Props) => {
             src={"https://i.imgur.com/4TG3qoW.png"}
             alt={"cats"}
           ></img>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">Keyboard cats</div>
+          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+            Keyboard cats
+          </div>
         </div>
         <div className="w-1/6 p-1 rotate-8 bg-white shadow-sm rounded-md transition-all duration-300 hover:rotate-0 hover:scale-125 ease-out hover:shadow-md hover:z-10">
           <div className="rounded-sm bg-stone-200 w-full h-3/4 relative">
             <UserIcon className="absolute h-2/3 w-2/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-stone-400" />
           </div>
-          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">You?</div>
+          <div className="text-xs text-center mt-1 font-serif italic text-stone-400">
+            You?
+          </div>
         </div>
       </div>
     </section>
